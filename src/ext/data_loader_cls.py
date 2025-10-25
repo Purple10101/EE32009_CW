3e490f7bc7e41bb5e9f43e400230353f233bfaf8
Purple10101 <joshpoole04@gmail.com>
2025-10-25 18:21:23 +0100

    20251025 - Better capture formatting and noise injection.

--- src/ext/data_loader_cls.py
+++ src/ext/data_loader_cls.py
@@ -38,14 +38,30 @@ class Recording:
         self.idx_lst = idx_lst[0]
         self.cls_lst = cls_lst[0]
 
+        # Noise gets added to the un-normalized time series
+        # and then gets normalized
+
         # processed variables
         self.idx_lst_processed = self.peak_idx_processing()
-        self.captures_all = self.split_spike()
+        self.captures_all, self.captures_train = self.split_spike()
+        # training has the stacked neurons removed
         self.captures_training, self.captures_test = self.split_caps()
 
+        self.captures_training_norm = self.norm_data(self.captures_training)
+        self.captures_test_norm = self.norm_data(self.captures_all)
+
     def split_caps(self, tr_to_tst_r=0.8):
-        split_index = int(len(self.captures_all) * tr_to_tst_r)
-        return self.captures_all[:split_index], self.captures_all[split_index:]
+        split_index = int(len(self.captures_train) * tr_to_tst_r)
+        return self.captures_train[:split_index], self.captures_train[split_index:]
+
+    def norm_data(self, dataset):
+        ret_val = copy.deepcopy(dataset)
+        for i, capture in enumerate(ret_val):
+            raw_data_max = max(capture["Capture"])
+            raw_data_min = min(capture["Capture"])
+            ret_val[i]["Capture"] = (2 * (capture["Capture"] - raw_data_min) /
+                (raw_data_max - raw_data_min) - 1)
+        return ret_val
 
     def noise_injection(self, dataset, snr_out, snr_in=80):
         ret_val = copy.deepcopy(dataset)
@@ -59,19 +75,21 @@ class Recording:
             ret_val[i]["Capture"] = capture["Capture"] + noise
         return ret_val
 
-    def split_spike(self, capture_width=60, capture_weight=0.9):
+    def split_spike(self, capture_width=80, capture_weight=0.90):
         """
         :param capture_width:  How many samples per capture
         :param capture_weight: A right bias value as the dataset
                                tends to house important data on the right of idx
         :return:               A list of all captures in format {Capture=[], Classification=int}
         """
-        captures_list = []
+        captures_list_tr = []
+        captures_list_all = []
         for classification_index, idx in enumerate(self.idx_lst_processed):
             cap = []
             if isinstance(idx, list):
                 # This is my fancy magic eraser like they advertise for pictures on your phone
                 # Only this time its for a second peak that's too damn close to the last one
+                decay_rate = 1
                 for sample_idx in range(int(idx[0]-(capture_width * (1 - capture_weight))),
                                         int(idx[0]+(capture_width * capture_weight))):
                     if sample_idx >= idx[1]:
@@ -79,21 +97,35 @@ class Recording:
                         # just keep rolling off with a lil bit of noise
                         # we add noise because else the CNN will go
                         # "bloody hell some of them have constant values after the spike!"
-                        inj_value = cap[abs(idx[1] - idx[0]) - 1]
-                        noisy_value = inj_value + (random.uniform(-1, 1) * abs(inj_value) * 0.25)
-                        cap.append(noisy_value)
+                        ##inj_value = cap[-1]
+                        ##inj_value /= decay_rate
+                        ##decay_rate += 0.001
+                        ##noisy_value = inj_value + (random.uniform(-1, 1) * abs(inj_value) * 0.3)
+                        ##cap.append(noisy_value)
+                        cap.append(self.raw_data[sample_idx])
                     else:
                         cap.append(self.raw_data[sample_idx])
+                # We will exclude this sample from training
+                captures_list_all.append({
+                    "Capture": cap,
+                    "Classification": self.cls_lst[classification_index],
+                    "PeakIdx": capture_width * (1 - capture_weight)
+                })
             else:
                 for sample_idx in range(int(idx-(capture_width * (1 - capture_weight))),
                                         int(idx+(capture_width * capture_weight))):
                     cap.append(self.raw_data[sample_idx])
-            captures_list.append({
-                "Capture": cap,
-                "Classification": self.cls_lst[classification_index],
-                "PeakIdx": capture_width * (1 - capture_weight)
-            })
-        return captures_list
+                captures_list_tr.append({
+                    "Capture": cap,
+                    "Classification": self.cls_lst[classification_index],
+                    "PeakIdx": capture_width * (1 - capture_weight)
+                })
+                captures_list_all.append({
+                    "Capture": cap,
+                    "Classification": self.cls_lst[classification_index],
+                    "PeakIdx": capture_width * (1 - capture_weight)
+                })
+        return captures_list_all, captures_list_tr
 
     def peak_idx_processing(self, capture_width=60):
         """
@@ -103,11 +135,12 @@ class Recording:
         :param capture_width:  How many samples per capture
         :return:
         """
-        ret_val = self.idx_lst.tolist()
+        ret_val = copy.deepcopy(self.idx_lst.tolist())
         for i in range(len(ret_val)):
-            for j in range(i + 1, len(self.idx_lst)):
+            for j in range(len(ret_val)):
                 if abs(self.idx_lst[i] - self.idx_lst[j]) <= capture_width and self.idx_lst[i] < self.idx_lst[j]:
-                    ret_val[i] = [ret_val[i], ret_val[j]]
+                    ret_val[i] = [self.idx_lst[i], self.idx_lst[j]]
+                    break
         return ret_val
 
 
---
+++ src/nn/n_cls_train.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+"""
+===========================================================
+ Title:        n_cls_train.py
+ Description:
+ Author:       Joshua Poole
+ Created on:   20251024
+ Version:      1.0
+===========================================================
+
+ Notes:
+    - So things that could be hampering the performance:
+        - Normalisation?
+            - Right now you norm each capture independently.
+              This could be an issue!
+        - More variations of the degraded training set?
+        - Shuffling the training set?
+        - Criterion and optimiser?
+        - Could you randomly deteriorate some samples?
+          so that its being fed one at 80 then one at 0 then one at 60
+
+ Requirements:
+    - Python >= 3.11
+    - scipy
+    - numpy
+    - matplotlib
+    - random
+
+==================
+"""
+
+from scipy.io import loadmat
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+
+from src.ext.data_loader_cls import Recording, plot_sample
+from src.nn.n_cls import NeuronCNN
+
+#os.chdir(os.path.dirname(os.path.dirname(os.getcwd())))
+
+def noise_plt_example(rec, snr_out):
+    # plot the 0th capture with noise injection
+    for snr in snr_out:
+        noisy_un_norm = rec.noise_injection(rec.captures_training, snr)
+        noisy_norm = rec.norm_data(noisy_un_norm)
+        plot_sample(noisy_norm[0])
+
+def prep_training_set(rec, snr_out=80):
+    if snr_out == 80:
+        captures = [d["Capture"] for d in rec.captures_training_norm]
+        clss = [d["Classification"] for d in rec.captures_training_norm]
+        # for example plot some normed time series
+        for i in range(30):
+            capture = rec.captures_training_norm[i]
+            if capture["Classification"] == 1:
+                plot_sample(capture)
+    else:
+        noisy_un_norm = rec.noise_injection(rec.captures_training, snr)
+        noisy_norm = rec.norm_data(noisy_un_norm)
+        captures = [d["Capture"] for d in noisy_norm]
+        clss = [d["Classification"] for d in noisy_norm]
+
+    X = np.array(captures, dtype=np.float32)
+    y = np.array(clss, dtype=np.int64)
+    X = np.expand_dims(X, axis=1)
+    X_tensor = torch.tensor(X)
+    y_tensor = torch.tensor(y)
+    dataset = TensorDataset(X_tensor, y_tensor)
+    return DataLoader(dataset, batch_size=32, shuffle=True)
+
+
+# data prep
+print(os.getcwd())
+data = loadmat('data\D1.mat')
+data_inf = loadmat('data\D2.mat')
+
+rec = Recording(data['d'], data['Index'], data['Class'])
+
+snr_lst = [80, 60, 20] # add back 0 and -10
+
+# show example of manual degradation
+noise_plt_example(rec, snr_lst)
+# now create test sets for each SNR
+training_sets = []
+for snr in snr_lst:
+    training_sets.append(prep_training_set(rec, snr))
+
+
+
+# model and training
+model = NeuronCNN(6)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.AdamW(model.parameters(), lr=10e-4, weight_decay=1e-4)
+
+num_epochs = 500
+
+for epoch in range(num_epochs):
+    model.train()
+    epoch_loss = 0.0
+    total_batches = 0
+
+    for dataloader in training_sets:
+        for X_batch, y_batch in dataloader:
+            optimizer.zero_grad()
+            preds = model(X_batch)
+            loss = criterion(preds, y_batch)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            optimizer.step()
+
+            epoch_loss += loss.item()
+            total_batches += 1
+
+    avg_loss = epoch_loss / total_batches
+    print(f"Epoch {epoch + 1}: avg_loss = {avg_loss:.4f}")
+
+torch.save(model.state_dict(), "src/nn/models/20251025_neuron_noise_inj_cls.pt")
+
+# separate training and inference, make n_cls_inf.py
+
+# load model and evaluate performance
+model = NeuronCNN(6)
+model.load_state_dict(torch.load("src/nn/models/20251025_neuron_noise_inj_cls.pt"))
+model.eval()
+
+scorecard = []
+
+with torch.no_grad():
+    for test_capture in rec.captures_test_norm:
+        X = np.array(test_capture["Capture"], dtype=np.float32)
+        X_min = X.min(axis=0)
+        X_max = X.max(axis=0)
+        X_norm = (X - X_min) / (X_max - X_min)
+        X = np.expand_dims(X, axis=1)
+        X_tensor = torch.tensor(X).T.unsqueeze(0)
+        outputs = model(X_tensor)
+        predicted = torch.argmax(outputs)
+        real_lb = test_capture["Classification"]
+        if predicted == real_lb:
+            scorecard.append(1)
+        else:
+            scorecard.append(0)
+            plot_sample(test_capture)
+scorecard_array = np.array(scorecard)
+print(f"performance = {scorecard_array.mean()*100}%")
+
+scorecard = []
+
+with torch.no_grad():
+    noisy_un_norm = rec.noise_injection(rec.captures_training, 0)
+    noisy_norm = rec.norm_data(noisy_un_norm)
+    for test_capture in noisy_norm:
+        X = np.array(test_capture["Capture"], dtype=np.float32)
+        X_min = X.min(axis=0)
+        X_max = X.max(axis=0)
+        X_norm = (X - X_min) / (X_max - X_min)
+        X = np.expand_dims(X, axis=1)
+        X_tensor = torch.tensor(X).T.unsqueeze(0)
+        outputs = model(X_tensor)
+        predicted = torch.argmax(outputs)
+        real_lb = test_capture["Classification"]
+        if predicted == real_lb:
+            scorecard.append(1)
+        else:
+            scorecard.append(0)
+scorecard_array = np.array(scorecard)
+print(f"performance = {scorecard_array.mean()*100}%")
